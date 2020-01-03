cc171c66bbe093d9010ee4adf7d25ac11e06bdda
eyuelberga <eyuel.berga@yahoo.com>
2020-01-03 21:23:11 +0300

    modification on templates..

--- schools/models.py
+++ schools/models.py
@@ -4,4 +4,8 @@ from django.contrib.gis.db import models
 class School(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField()
-    
+
+
+class ResidentialArea(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.MultiPolygonField()
--- schools/views.py
+++ schools/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
 from django.views import generic
 from django.views.generic import DetailView
-from django.contrib.gis.geos import fromstr
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 from .models import School
-from pathlib import Path
-import json
-from html import unescape
 
-RESIDENTIAL_AREA = 'residential_areas.js'
-redidential_area_jsonfile = Path(__file__).parents[2] / RESIDENTIAL_AREA
 longitude = 38.7494272
 latitude = 8.943757
 user_location = Point(longitude, latitude, srid=4326)
@@ -22,20 +15,21 @@ class Home(generic.ListView):
     template_name = 'schools/dashboard.html'
     queryset = School.objects.all()
 
+
 class Schools(generic.ListView):
     model = School
     context_object_name = 'schools'
-    # queryset = School.objects.annotate(distance=Distance('location', user_location)).order_by('distance')
     queryset = School.objects.all()
     template_name = 'schools/schools.html'
+
+
+class DistanceAnalysis(generic.ListView):
+    model = School
+    context_object_name = 'schools'
+    queryset = School.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6]
+    template_name = 'schools/distance_analysis.html'
 
 
 class SchoolsDetailView(DetailView):
     template_name = 'schools/detail.html'
     model = School
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['layer_residential_areas'] = unescape(open(redidential_area_jsonfile, 'r').read())
-
-        return context
