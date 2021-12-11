147a28188da210ace4e8b277786c5d64b27426ec
eyuelberga <30384633+eyuelberga@users.noreply.github.com>
2021-12-11 11:54:01 +0300

    ci: update aptFile

--- new_school_locations/settings.py
+++ new_school_locations/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
